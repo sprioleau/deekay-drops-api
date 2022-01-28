baebee1b1a5de2499f3348b5a17b33e792b965fd
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-01-28 06:57:19 -0500

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# An \[Unofficial\] Deekay Drops API
+
+_This repo deploys a serveless function for scraping image sources from the [DeeKay Kwon Let's Walk Series](https://deekaykwon.com/letswalk)._
+
+He's an amazing creator/animator and I wanted to reference his work to build out starter projects to test out some full-stack frameworks.
---
+++ functions/drops.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const scrapeUrl = "https://deekaykwon.com/letswalk";
+
+exports.handler = async (event, context) => {
+	const { data: html } = await axios.get(scrapeUrl);
+	const $ = cheerio.load(html);
+	const images = [];
+
+	$(".image-block-wrapper img").each((i, image) => {
+		const src = $(image).attr("data-image");
+		const alt = $(image).attr("alt");
+		const [name, number] = alt.split(" #");
+
+		if (alt) images.push({ src, alt, name, number });
+	});
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify(images),
+	};
+};
